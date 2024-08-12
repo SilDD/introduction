4234d10fe0cdcdd2fb0cf3282bb16e52cfe2d080
Johannes Klaus <johannes.k-buero@gmx.de>
2024-08-12 21:07:22 +0200

    create introduction block

---
+++ introduction/home/templatetags/custom_tags.py
@@ -0,0 +1,21 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+import bleach
+
+register = template.Library()
+
+
+@register.filter(name='allow_safe_tags')
+@template.defaultfilters.stringfilter
+def allow_safe_tags(value):
+    """
+    Erlaubt nur bestimmte HTML-Tags (z.B. <b>, <br>, <i>, <u>) und rendert den Text sicher.
+    """
+    if value:
+        # Liste der erlaubten Tags erweitern
+        allowed_tags = ['b', 'br']
+        cleaned_value = bleach.clean(value, tags=allowed_tags, strip=True)
+        # Markiert den bereinigten Text als sicher
+        return mark_safe(cleaned_value)
+    return value
