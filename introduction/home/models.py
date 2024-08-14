c07426c127aed235d31355322a9f3d65ae73472a
Johannes Klaus <johannes.k-buero@gmx.de>
2024-08-15 01:37:51 +0200

    create-page-settings

--- introduction/home/models.py
+++ introduction/home/models.py
@@ -1,10 +1,44 @@
+from tabnanny import verbose
+
 from django.db import models
-from wagtail.admin.panels import FieldPanel
+
 from wagtail.documents.models import Document
 from wagtail.fields import StreamField
 from wagtail.models import Page
 
 from home.blocks import LandingBlock, Introduction, OrderedProjects, Motivation, SkillChoiceBlock
+
+from wagtail.admin.panels import (
+    FieldPanel,
+    MultiFieldPanel
+
+)
+from wagtail.contrib.settings.models import (
+    BaseGenericSetting,
+    register_setting,
+)
+
+
+@register_setting
+class NavigationSettings(BaseGenericSetting):
+    first_name = models.CharField(verbose_name="First name", blank=True, max_length=20)
+    last_name = models.CharField(verbose_name="Last name", blank=True, max_length=20)
+    linkedin_url = models.URLField(verbose_name="LinkedIn URL", blank=True)
+    github_url = models.URLField(verbose_name="GitHub URL", blank=True)
+    email = models.EmailField(verbose_name="E-Mail_Adress", blank=True)
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel('first_name'),
+                FieldPanel('last_name'),
+                FieldPanel("linkedin_url"),
+                FieldPanel("github_url"),
+                FieldPanel("email"),
+            ],
+            "Social settings",
+        )
+    ]
 
 
 class HomePage(Page):
