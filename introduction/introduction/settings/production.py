f9664242a2e782c9becd48f74329af66d3f7f13b
Johannes Klaus <johannes.k-buero@gmx.de>
2024-09-27 22:20:58 +0200

    set scss paths

--- introduction/introduction/settings/production.py
+++ introduction/introduction/settings/production.py
@@ -8,6 +8,7 @@ SECRET_KEY = "django-insecure-e11-2_)rqx#-(x^*62h#(i296q%(xwh(4sz$&2m@822lbvzs7=
 ALLOWED_HOSTS = ["johannesklaus.me"]
 
 CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
+ROOT_URLCONF = "introduction.urls"
 
 DATABASES = {
     'default': {
