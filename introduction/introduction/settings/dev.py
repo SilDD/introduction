5e55230822a31d573ba0f86935f37c3e2e68cfc4
Johannes Klaus <johannes.k-buero@gmx.de>
2024-09-27 20:10:41 +0200

    set scss paths

--- introduction/introduction/settings/dev.py
+++ introduction/introduction/settings/dev.py
@@ -7,9 +7,16 @@ DEBUG = True
 SECRET_KEY = "django-insecure-e11-2_)rqx#-(x^*62h#(i296q%(xwh(4sz$&2m@822lbvzs7="
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['www.johannesklaus.me', 'johannesklaus.me']
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Stelle sicher, dass der Pfad korrekt ist
+    }
+}
 
 
 try:
