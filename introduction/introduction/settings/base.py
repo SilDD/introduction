b8f880c479397491f6d0e11a30a0d32f858c07ba
Johannes Klaus <johannes.k-buero@gmx.de>
2024-09-27 20:34:52 +0200

    set scss paths

--- introduction/introduction/settings/base.py
+++ introduction/introduction/settings/base.py
@@ -16,7 +16,7 @@ import os
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
-
+ALLOWED_HOSTS = ['www.johannesklaus.me']
 
 
 # Quick-start development settings - unsuitable for production
