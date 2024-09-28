a6934616637b09131d04f99918d16ab6a26eece1
Johannes Klaus <johannes.k-buero@gmx.de>
2024-09-28 11:11:00 +0200

    set scss paths

--- introduction/introduction/settings/production.py
+++ introduction/introduction/settings/production.py
@@ -10,13 +10,6 @@ ALLOWED_HOSTS = ["johannesklaus.me"]
 CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Stelle sicher, dass der Pfad korrekt ist
-    }
-}
-
 
 try:
     from .local import *
