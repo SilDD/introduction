d78a1f4381574b9bde82dcfed55acbe3a5f6f497
Johannes Klaus <johannes.k-buero@gmx.de>
2024-10-18 10:19:55 +0200

    impressum

--- introduction/introduction/settings/base.py
+++ introduction/introduction/settings/base.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'wagtail_modeladmin',
 
     'wagtailmenus',
+    'richtextpage',
     "home",
     "streams",
     "search",
---
+++ introduction/richtextpage/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+
+class RichTextPage(Page):
+    template = 'richtext.html'
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
